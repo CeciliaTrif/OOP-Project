7129a1b73f371dcc77b0661f3d9b10af531d5a59
Trif Cecilia <trifceci@gmail.com>
2022-03-19 19:47:08 +0200

    - initial commit

---
+++ cmake-build-debug/CMakeFiles/ProiectOOP.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/trifc/CLionProjects/ProiectOOP/Car.cpp" "C:/Users/trifc/CLionProjects/ProiectOOP/cmake-build-debug/CMakeFiles/ProiectOOP.dir/Car.cpp.obj"
+  "C:/Users/trifc/CLionProjects/ProiectOOP/CarValidator.cpp" "C:/Users/trifc/CLionProjects/ProiectOOP/cmake-build-debug/CMakeFiles/ProiectOOP.dir/CarValidator.cpp.obj"
+  "C:/Users/trifc/CLionProjects/ProiectOOP/FileRepository.cpp" "C:/Users/trifc/CLionProjects/ProiectOOP/cmake-build-debug/CMakeFiles/ProiectOOP.dir/FileRepository.cpp.obj"
+  "C:/Users/trifc/CLionProjects/ProiectOOP/Repository.cpp" "C:/Users/trifc/CLionProjects/ProiectOOP/cmake-build-debug/CMakeFiles/ProiectOOP.dir/Repository.cpp.obj"
+  "C:/Users/trifc/CLionProjects/ProiectOOP/TestCar.cpp" "C:/Users/trifc/CLionProjects/ProiectOOP/cmake-build-debug/CMakeFiles/ProiectOOP.dir/TestCar.cpp.obj"
+  "C:/Users/trifc/CLionProjects/ProiectOOP/TestFileRepository.cpp" "C:/Users/trifc/CLionProjects/ProiectOOP/cmake-build-debug/CMakeFiles/ProiectOOP.dir/TestFileRepository.cpp.obj"
+  "C:/Users/trifc/CLionProjects/ProiectOOP/TestRepository.cpp" "C:/Users/trifc/CLionProjects/ProiectOOP/cmake-build-debug/CMakeFiles/ProiectOOP.dir/TestRepository.cpp.obj"
+  "C:/Users/trifc/CLionProjects/ProiectOOP/TestUtilityVehicle.cpp" "C:/Users/trifc/CLionProjects/ProiectOOP/cmake-build-debug/CMakeFiles/ProiectOOP.dir/TestUtilityVehicle.cpp.obj"
+  "C:/Users/trifc/CLionProjects/ProiectOOP/TestVehicle.cpp" "C:/Users/trifc/CLionProjects/ProiectOOP/cmake-build-debug/CMakeFiles/ProiectOOP.dir/TestVehicle.cpp.obj"
+  "C:/Users/trifc/CLionProjects/ProiectOOP/TestVehicleValidator.cpp" "C:/Users/trifc/CLionProjects/ProiectOOP/cmake-build-debug/CMakeFiles/ProiectOOP.dir/TestVehicleValidator.cpp.obj"
+  "C:/Users/trifc/CLionProjects/ProiectOOP/UtilityVehicle.cpp" "C:/Users/trifc/CLionProjects/ProiectOOP/cmake-build-debug/CMakeFiles/ProiectOOP.dir/UtilityVehicle.cpp.obj"
+  "C:/Users/trifc/CLionProjects/ProiectOOP/UtilityVehicleValidator.cpp" "C:/Users/trifc/CLionProjects/ProiectOOP/cmake-build-debug/CMakeFiles/ProiectOOP.dir/UtilityVehicleValidator.cpp.obj"
+  "C:/Users/trifc/CLionProjects/ProiectOOP/Utils.cpp" "C:/Users/trifc/CLionProjects/ProiectOOP/cmake-build-debug/CMakeFiles/ProiectOOP.dir/Utils.cpp.obj"
+  "C:/Users/trifc/CLionProjects/ProiectOOP/Vehicle.cpp" "C:/Users/trifc/CLionProjects/ProiectOOP/cmake-build-debug/CMakeFiles/ProiectOOP.dir/Vehicle.cpp.obj"
+  "C:/Users/trifc/CLionProjects/ProiectOOP/VehicleValidator.cpp" "C:/Users/trifc/CLionProjects/ProiectOOP/cmake-build-debug/CMakeFiles/ProiectOOP.dir/VehicleValidator.cpp.obj"
+  "C:/Users/trifc/CLionProjects/ProiectOOP/main.cpp" "C:/Users/trifc/CLionProjects/ProiectOOP/cmake-build-debug/CMakeFiles/ProiectOOP.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ProiectOOP.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProiectOOP.dir/Car.cpp.obj"
+  "CMakeFiles/ProiectOOP.dir/CarValidator.cpp.obj"
+  "CMakeFiles/ProiectOOP.dir/FileRepository.cpp.obj"
+  "CMakeFiles/ProiectOOP.dir/Repository.cpp.obj"
+  "CMakeFiles/ProiectOOP.dir/TestCar.cpp.obj"
+  "CMakeFiles/ProiectOOP.dir/TestFileRepository.cpp.obj"
+  "CMakeFiles/ProiectOOP.dir/TestRepository.cpp.obj"
+  "CMakeFiles/ProiectOOP.dir/TestUtilityVehicle.cpp.obj"
+  "CMakeFiles/ProiectOOP.dir/TestVehicle.cpp.obj"
+  "CMakeFiles/ProiectOOP.dir/TestVehicleValidator.cpp.obj"
+  "CMakeFiles/ProiectOOP.dir/UtilityVehicle.cpp.obj"
+  "CMakeFiles/ProiectOOP.dir/UtilityVehicleValidator.cpp.obj"
+  "CMakeFiles/ProiectOOP.dir/Utils.cpp.obj"
+  "CMakeFiles/ProiectOOP.dir/Vehicle.cpp.obj"
+  "CMakeFiles/ProiectOOP.dir/VehicleValidator.cpp.obj"
+  "CMakeFiles/ProiectOOP.dir/main.cpp.obj"
+  "ProiectOOP.exe"
+  "ProiectOOP.exe.manifest"
+  "ProiectOOP.pdb"
+  "libProiectOOP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProiectOOP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
